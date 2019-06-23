b897b2111bf63b9b63d23e3fc8f5bb3d9931e77e
Enes Kahraman <official.phoique@gmail.com>
2019-06-23 22:40:52 +0300

    movies comment line

--- routes/myMovies.js
+++ routes/myMovies.js
@@ -6,11 +6,15 @@ import userModel from '../models/User';
 
 const myMoviesRoute = express.Router();
 
+// filmleri listeleme sayfasının ilk sayfası(page)
 myMoviesRoute.get('/', (request, response) => {
 
+  // Kullanıcının filmlerini arıyor.
   const user = userModel.findOne({ username: request.session.username });
 
+  // Filmleri listeliyor.
   user.then(user_id => {
+    // Paginate işlemi yapılıyor limit 12 sayfa ise 1 
     const movies = movieModel.paginate({ user_id }, { page: 1, limit: 12 });
 
     movies.then(movie => {
@@ -20,7 +24,8 @@ myMoviesRoute.get('/', (request, response) => {
         username: request.session.username,
         user_role: request.session.role,
         movies: movie.docs,
-        pages_number: Array(movie.pages).fill(0).map((e,i)=>i+1)
+        // sayfa numarası 2 veya büyük ise 1,2,3.. numaralar çıkıyor.
+        pages_number: (movies.pages >= 2) ? Array(movie.pages).fill(0).map((e,i)=>i+1) : null
       });
     });
     movies.catch(error => console.log(error));
@@ -30,11 +35,15 @@ myMoviesRoute.get('/', (request, response) => {
 
 });
 
+// Sayfalamada 2 ve üzeri bir sayfa var ise burası çalışıyor. 
 myMoviesRoute.get('/:page', (request, response) => {
 
   const user = userModel.findOne({ username: request.session.username });
 
   user.then(user_id => {
+
+    // Kullanıcının alk kısımda tıkladığı 1 2 3.. numaralar /1 /2 şeklinde o numara kaç ise o
+    // sayfa açılıyor.
     const movies = movieModel.paginate({ user_id }, { page: request.params.page, limit: 12 });
 
     movies.then(movie => {
@@ -45,7 +54,7 @@ myMoviesRoute.get('/:page', (request, response) => {
         username: request.session.username,
         user_role: request.session.role,
         movies: movie.docs,
-        pages_number: Array(movie.pages).fill(0).map((e,i)=>i+1)
+        pages_number: (movies.pages >= 2) ? Array(movie.pages).fill(0).map((e,i)=>i+1) : null
       });
     });
     movies.catch(error => console.log(error));
