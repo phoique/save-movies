50ecc941ee2f8e05db830fb1350cbfef4a267fe7
Enes Kahraman <official.phoique@gmail.com>
2019-07-13 11:58:50 +0300

    Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Components
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+
+// CSS
 import './assets/css/styles.css';
 
 function App() {
   return (
     <div className="container">
       <Navbar />
-      <Login />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path ="/register" exact component={Register} />
+        </Switch>
+      </Router>
     </div>
-
   );
 }
 
