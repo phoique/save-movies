4c83789a064ef961915bc9abdc7d99670db6e8e2
Enes Kahraman <official.phoique@gmail.com>
2019-07-20 01:53:30 +0300

    home bug fixed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,10 +4,22 @@ function Home() {
   return (
     <header className="row header-main text-center mt-5">
       <h1 className="col-md-12 col-sm-12 display-5 font-weight-bold">İzlediğin filmleri kaydetmek çok kolay.</h1>
-      <p className="col-md-12 col-sm-12 header-text text-secondary">İzlediğin filmi mi kaydetmek istiyorsun, yoksa arkadaşın ile paylaşmak mı? Daha fazlası için lütfen kayıt ol ve keyfini çıkar.</p>
-      <a className="col-md-12 col-sm-12" href="/register">
-        <button type="button" className="btn btn-primary header-register font-weight-bold">Kayıt Ol</button>
-      </a>
+      <p className="col-md-12 col-sm-12 header-text text-secondary">
+        {
+          (localStorage.getItem('token')) ? 
+          "İzlediğin filmi mi kaydetmek istiyorsun, hadi o zaman hemen kaydet." 
+          :
+          "İzlediğin filmi mi kaydetmek istiyorsun, yoksa arkadaşın ile paylaşmak mı? Daha fazlası için lütfen kayıt ol ve keyfini çıkar."
+        }
+      </p>
+      {
+        (localStorage.getItem('token')) ? 
+        null 
+        :
+        <a className="col-md-12 col-sm-12" href="/register">
+          <button type="button" className="btn btn-primary header-register font-weight-bold">Kayıt Ol</button>
+        </a>
+      }
     </header>
   );
 }
