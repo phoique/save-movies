4b1a401e47841c61bb24ec85ca970deebe14fb36
Phoique <official.phoique@gmail.com>
2020-01-13 04:22:54 +0300

    is login route bug fix

--- client/src/utils/IsLoginRoute.js
+++ client/src/utils/IsLoginRoute.js
@@ -4,7 +4,9 @@ import { Route, Redirect } from 'react-router-dom';
 const IsLoginRoute = ({token, component: Component, ...rest }) => (
   <Route {...rest} render={
     props => (
-      (token !== null) ? 
+      // gelen token objesinde anahtar yok ise yani kullanıcı bilgilerine dair bir şey
+      // yoksa giriş yapmamış demektir.
+      (Object.keys(token).length !== 0) ? 
       <Redirect to="/" />
       : 
       <Component {...props} />
