322c945b416289e2aecb46e755a1a5b8d6593ab8
Phoique <official.phoique@gmail.com>
2020-01-14 04:07:45 +0300

    logout token context api delete

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
+import { getToken } from '../actions/token';
+import { Context } from '../store/store';
 
 function Logout() {
+  const { dispatch } = useContext(Context);
+  // Kullanıcı çıkış yaptıktan sonra token siliniyor.
+  dispatch(getToken(null));
   localStorage.clear();
+
   return (
     <Redirect to="/" />
   );
