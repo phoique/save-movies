38ea9bc8e0bd46d1c050b831838118ffe8280bc4
Phoique <official.phoique@gmail.com>
2020-01-19 02:07:59 +0300

    User operations action and reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import auth from './authReducer';
 import myMovies from './myMoviesReducer';
 import addMovie from './addMovieReducer';
+import users from './usersReducer';
 
 export default combineReducers({
   authToken: auth,
   myMovies: myMovies,
-  newMovie: addMovie
+  newMovie: addMovie,
+  users: users
 });
---
+++ client/src/reducers/usersReducer.js
@@ -0,0 +1,85 @@
+import {
+  FETCH_USER_PENDING,
+  FETCH_USER_FULFILLED,
+  FETCH_USER_REJECTED,
+
+  DELETE_USER_PENDING,
+  DELETE_USER_FULFILLED,
+  DELETE_USER_REJECTED,
+
+  PERM_USER_PENDING,
+  PERM_USER_FULFILLED,
+  PERM_USER_REJECTED
+ } from '../actions/users';
+
+// Default state
+const initialState = {
+  fetching: false,
+  users: [],
+  permissionUser: [],
+  deleteUser: [],
+  page: null,
+  error: {},
+}
+
+// Reducer
+export default (state = initialState, { type, payload }) => {
+	switch (type){
+    // Kullanıcı getirme
+		case FETCH_USER_PENDING:
+			return {
+				...state,
+				fetching: true
+			};
+		case FETCH_USER_FULFILLED:
+			return {
+				...state,
+				users: payload.data.users,
+				page: payload.data.pages_number,
+				fetching: false
+			};
+		case FETCH_USER_REJECTED:
+			return {
+				error: payload,
+				fetching: false
+      };
+      
+    // Kullanıcı silme
+		case DELETE_USER_PENDING:
+			return {
+				...state,
+				fetching: true
+			};
+		case DELETE_USER_FULFILLED:
+			return {
+				...state,
+				deleteUser: payload.data.delete_user,
+				fetching: false
+			};
+		case DELETE_USER_REJECTED:
+			return {
+				error: payload,
+				fetching: false
+      };
+    
+    // Kullanıcı yetkilendirme
+		case PERM_USER_PENDING:
+			return {
+				...state,
+				fetching: true
+			};
+		case PERM_USER_FULFILLED:
+			return {
+				...state,
+				permissionUser: payload.data.update_user,
+				fetching: false
+			};
+		case PERM_USER_REJECTED:
+			return {
+				error: payload,
+				fetching: false
+			};
+		default:
+      return state;
+  }
+}
