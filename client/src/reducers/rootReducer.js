7e3f6e32a18a97b0e413f6c47c53210efb7625ea
Phoique <official.phoique@gmail.com>
2020-01-19 03:24:54 +0300

    public check action and reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -4,10 +4,12 @@ import auth from './authReducer';
 import myMovies from './myMoviesReducer';
 import addMovie from './addMovieReducer';
 import users from './usersReducer';
+import check from './publicCheckReducer';
 
 export default combineReducers({
   authToken: auth,
   myMovies: myMovies,
   newMovie: addMovie,
-  users: users
+  users: users,
+  checkMovie: check
 });
